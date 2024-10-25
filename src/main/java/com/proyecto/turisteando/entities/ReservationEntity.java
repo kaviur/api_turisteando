1867cb04f09c832ac847144729e8e1074c6eee07
Ana Carolina Gonzalez Alzate <AnaCa@MacBook-Pro-de-diego.local>
2024-10-24 19:57:53 -0500

    feat(reservation): add entity

---
+++ src/main/java/com/proyecto/turisteando/entities/ReservationEntity.java
@@ -0,0 +1,44 @@
+package com.proyecto.turisteando.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "reservation")
+public class ReservationEntity {
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_reservation")
+    private int idReservation;
+
+    // Relación con la entidad User
+    @ManyToOne
+    @JoinColumn(name = "id_user", referencedColumnName = "id_user", nullable = false)
+    private User user;
+
+    // Relación con la entidad TouristPlan
+    @ManyToOne
+    @JoinColumn(name = "id_plan", referencedColumnName = "id_plan", nullable = false)
+    private TouristPlan plan;
+
+    @Column(name = "reservation_date", nullable = false)
+    private LocalDateTime reservationDate;
+
+    @Column(name = "status", nullable = false)
+    private String status; // Confirmed, Pending, Cancelled
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+}

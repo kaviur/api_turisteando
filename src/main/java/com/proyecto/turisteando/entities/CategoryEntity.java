bc709e93fb2b6fe7d22fef2c11ee88c689221043
karen <quemojojojo@gmail.com>
2024-10-26 01:03:25 -0500

    feat: Full category crud including dtos and mappers, response class, global exception handler. Including mapping dependencies, logs, swagger, validations and environment variables

---
+++ src/main/java/com/proyecto/turisteando/entities/CategoryEntity.java
@@ -0,0 +1,49 @@
+package com.proyecto.turisteando.entities;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * Entity class representing a category.
+ * @author Karen Urbano - <a href="https://github.com/kaviur">kaviur</a>
+ * @version 1.0
+ * @since 2024-10-25
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "category")
+public class CategoryEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    @NotEmpty(message = "El nombre de la categoría no puede estar vacío")
+    private String name;
+
+    @Column(nullable = false) // it has no effect, but it is good to have it for clarity
+    private Byte status;
+
+    @Size(max = 255, message = "La descripción de la categoría no puede tener más de 255 caracteres")
+    private String description;
+
+    private String image;
+
+
+    @PrePersist
+    protected void onCreate() {
+        this.status = 1;
+    }
+
+}
+

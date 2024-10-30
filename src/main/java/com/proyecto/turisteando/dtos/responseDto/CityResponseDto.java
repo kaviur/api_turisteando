30590f453bc8d7275d9a4c262a84842deb6cbd10
HansUrpay <hansurpayh@gmail.com>
2024-10-29 22:13:02 -0500

    feat(city): update dtos, entity and controllers

--- src/main/java/com/proyecto/turisteando/dtos/responseDto/CityResponseDto.java
+++ src/main/java/com/proyecto/turisteando/dtos/responseDto/CityResponseDto.java
@@ -1,13 +1,18 @@
 package com.proyecto.turisteando.dtos.responseDto;
 
+import com.proyecto.turisteando.dtos.CountryDto;
 import com.proyecto.turisteando.dtos.IDto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 import java.io.Serializable;
 
+@Data
+@AllArgsConstructor
 public class CityResponseDto implements IDto, Serializable {
 
     private Long id;
     private String name;
-    private Long countryId;
+    private CountryDto country;
 
 }

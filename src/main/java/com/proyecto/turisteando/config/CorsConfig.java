6eca3e38a4c3817f3d545fdfa9eaf8a7a51f53a6
HansUrpay <hansurpayh@gmail.com>
2024-11-26 17:39:12 -0500

    feat(cors): add new cors config

--- src/main/java/com/proyecto/turisteando/config/CorsConfig.java
+++ src/main/java/com/proyecto/turisteando/config/CorsConfig.java
@@ -9,24 +9,25 @@ import org.springframework.web.filter.CorsFilter;
 
 import java.util.List;
 
-@Configuration
+//@Configuration
 public class CorsConfig {
 
-    @Value("${FRONTEND_URLS}")
-    private String frontendUrls;
-
-    @Bean
-    public CorsFilter corsFilter() {
-        List<String> frontendUrlsList = List.of(frontendUrls.split(","));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.setAllowedOrigins(frontendUrlsList);
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+//    @Value("${FRONTEND_URLS}")
+//    private String frontendUrls;
+//
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        List<String> frontendUrlsList = List.of(frontendUrls.split(","));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowCredentials(true);
+////        config.setAllowedOrigins(frontendUrlsList);
+//        config.addAllowedOrigin("*");
+//        config.addAllowedHeader("*");
+//        config.addAllowedMethod("*");
+//        source.registerCorsConfiguration("/**", config);
+//        return new CorsFilter(source);
+//    }
 
 }

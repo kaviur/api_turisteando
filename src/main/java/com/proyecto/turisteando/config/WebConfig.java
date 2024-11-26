8262fd4aad4766a31e64c47529e21875eeec32d8
HansUrpay <hansurpayh@gmail.com>
2024-11-26 17:58:05 -0500

    update(cors): update cors config

--- src/main/java/com/proyecto/turisteando/config/WebConfig.java
+++ src/main/java/com/proyecto/turisteando/config/WebConfig.java
@@ -1,13 +1,19 @@
 package com.proyecto.turisteando.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${FRONTEND_URLS}")
+    private String frontendUrls;
+
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowedOrigins(frontendUrls.split(","))
                 .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
